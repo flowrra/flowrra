29f6f1d200832b39bfab21a9e335b93166820ef1
Ahmad Ameen <ahmadmameen7@gmail.com>
2026-01-20 16:19:51 +0100

    fix: fix scheduler not executing tasks on first run

--- src/flowrra/__init__.py
+++ src/flowrra/__init__.py
@@ -39,7 +39,7 @@ Advanced usage with IOExecutor and CPUExecutor:
     )
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from flowrra.app import Flowrra
 from flowrra.task import Task, TaskResult, TaskStatus
